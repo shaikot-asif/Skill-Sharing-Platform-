73655f09a45674367a66cf6402ecb4d7ccb9e731
shaikot-asif <asif522666@gmail.com>
2024-03-30 16:40:40 +0600

    update user profile info and picture

---
+++ middleware/uploadPirctureMiddleware.js
@@ -0,0 +1,31 @@
+import { fileURLToPath } from "url";
+
+import multer from "multer";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../uploads"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const uploadPicture = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1 * 1000000, // 1MB
+  },
+  fileFilter: function (req, file, cb) {
+    let ext = path.extname(file.originalname);
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
+      return cb(new Error("Only images are allowed"));
+    }
+    cb(null, true);
+  },
+});
+
+export { uploadPicture };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,8 @@ import {
   registerUser,
   loginUser,
   userProfile,
+  updateProfile,
+  updateProfilePicture,
 } from "../controllers/userControllers.js";
 
 import { authGuard } from "../middleware/authMiddleware.js";
@@ -11,5 +13,7 @@ import { authGuard } from "../middleware/authMiddleware.js";
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile", authGuard, userProfile);
+router.put("/updateProfile", authGuard, updateProfile);
+router.put("/updateProfilePicture", authGuard, updateProfilePicture);
 
 export default router;
